97ebee0840bc197df2acd5b46cf7267ce2673a23
Matt Traudt <sirmatt@ksu.edu>
2017-03-10 00:20:45 -0500

    Huge overhaul ...
    - event-driven main loop that select()s from
      channel out messages and server out messages
    - don't use 'global' so much
    - add more logging functions
    - use more format strings instead of str+str+str
    - revamp highlight spam detection
       - add list of punc that can't be in a nick and strip it from
         the left and right in order to catch obfuscated nicks
    - make members list more accurate: watches for joins/parts/quits better

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-import sys
-import subprocess
+import selectors
 import signal
+import subprocess
+import sys
 from datetime import datetime
 
+selector = selectors.DefaultSelector()
+
 nick_prefixes = ['@','+']
-mention_limit = 10
+mention_limit = 1
 server_dir = None
 channel_name = None
 server_out = None
@@ -27,18 +30,23 @@ common_words = \
 'how','our','work','first','well','way','even','new','want','because','any',
 'these','give','day','most','us']
 
-mention_suffixes = [':',',','!']
+non_nick_punctuation = [':',',','!','?']
 
 def usage(prog_name):
     print(prog_name,"<root-ii-server-dir> <channel>")
 
+def log(fd, level, s):
+    assert fd
+    ts = datetime.now()
+    fd.write('[{}] [{}] {}\n'.format(ts, level, s))
+
 def log_debug(s):
     assert debug_fd
-    debug_fd.write("[{}] {}\n".format(datetime.now(),s))
+    return log(debug_fd, 'debug', s)
 
 def log_notice(s):
     assert notice_fd
-    notice_fd.write("[{}] {}\n".format(datetime.now(),s))
+    return log(notice_fd, 'notice', s)
 
 def log_warn(s):
     return log_notice(s)
@@ -56,13 +64,15 @@ def sigint(signum, stack_frame):
     server_out, channel_out = None, None
     if notice_fd:
         log_notice("Shutting down bot due to signal")
-    notice_fd.close()
+        notice_fd.close()
     if debug_fd:
         debug_fd.close()
     notice_fd, debug_fd = None, None
     exit(0)
 
 def sighup(signum, stack_frame):
+    if server_out: server_out.stdout.flush()
+    if channel_out: channel_out.stdout.flush()
     if notice_fd: notice_fd.flush()
     if debug_fd: debug_fd.flush()
 
@@ -70,25 +80,29 @@ def akick(nick, reason=None):
     with open(server_dir+'/in','w') as server_in:
         if not reason:
             log_notice('akicking {}'.format(nick))
-            server_in.write('/privmsg chanserv akick {} add {}!*@*\n'.format(
-                channel_name, nick))
+            #server_in.write('/privmsg chanserv akick {} add {}!*@*\n'.format(
+            #    channel_name, nick))
         else:
             log_notice('akicking {} for {}'.format(nick, reason))
-            server_in.write('/privmsg chanserv akick {} add {}!*@* {}\n'.format(
-                channel_name, nick, reason))
+            #server_in.write('/privmsg chanserv akick {} add {}!*@* {}\n'.format(
+            #    channel_name, nick, reason))
 
-def is_highlight_spam(words, members):
+def is_highlight_spam(words):
     words = [ w.lower() for w in words ]
     words = [ w for w in words if w not in common_words ]
     matches = set()
+    # first try straight nick mentions with no prefix/suffix obfuscation
     for match in [ w for w in words if w in members ]:
         matches.add(match)
-    for suff in mention_suffixes:
-        for match in [ m for m in members if m+suff in words]:
-            matches.add(match)
-    num_nicks = len(matches)
-    log_debug("{} nicks mentioned".format(num_nicks))
-    if num_nicks > mention_limit: return True
+    if len(matches) > mention_limit: return True
+    # then try removing leading/trailing punctuation from words and see if
+    # they then start to look like nicks. Not all punctuation is illegal
+    punc = ''.join(non_nick_punctuation)
+    for word in words:
+        word = word.lstrip(punc).rstrip(punc)
+        if word in members: matches.add(word)
+    log_debug("{} nicks mentioned".format(len(matches)))
+    if len(matches) > mention_limit: return True
     return False
 
 def contains_banned_word(words):
@@ -97,93 +111,134 @@ def contains_banned_word(words):
         if banned_word in words: return True
     return False
 
-def num_needles_in_haystack(hay, needles):
-    found = set()
-    for h in hay:
-        if h in needles: found.add(h)
-    return len(found)
-
 def get_all_members():
-    global server_out
     members = set()
-    server_out = subprocess.Popen(['tail','-F','-n','0',server_dir+'/out'],
-        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    with open(server_dir+'/in', 'w') as server_in:
-        server_in.write('/names '+channel_name+'\n')
+    with open('{}/in'.format(server_dir), 'w') as server_in:
+        server_in.write('/names {}\n'.format(channel_name))
     while True:
         line = server_out.stdout.readline().decode('utf8')
         tokens = line.split()
+        # check if line is one that lists nicks in our channel. Starts with @ or
+        #  =, then our channel name, then some nicks
         if tokens[2] in ['@', '='] and tokens[3] == channel_name:
             nicks = tokens[4:]
+            # some nicks have prefixes for ops. Remove those prefixes
             for n in nicks:
                 if n[0] in nick_prefixes: members.add(n[1:])
                 else: members.add(n)
+        # stop when we've found the end of the list
         elif tokens[2] == channel_name and tokens[3] == 'End' and \
             tokens[5] == '/NAMES':
             break
-    server_out.terminate()
-    server_out = None
     return members
+
+def member_add(nick):
+    global members
+    old_len = len(members)
+    members.add(nick.lower())
+    if len(members) <= old_len:
+        log_warn('Adding {} to members didn\'t increase length'.format(nick))
+
+def member_remove(nick):
+    global members
+    old_len = len(members)
+    members.discard(nick.lower())
+    if len(members) >= old_len:
+        log_warn('Removing {} from members didn\'t decrease length'.format(
+            nick))
+
+def member_changed_nick(old_nick, new_nick):
+    global members
+    old_nick = old_nick.lower()
+    new_nick = new_nick.lower()
+    # we only want to add the new nick if the old nick was in our set
+    old_len = len(members)
+    member_remove(old_nick)
+    if len(members) < old_len:
+        member_add(new_nick)
+        log_debug('{} --> {}'.format(old_nick, new_nick))
+
+def server_out_read_event(fd, mask):
+    line = fd.readline().decode('utf8')
+    tokens = line.split()
+    speaker = tokens[2]
+    words = tokens[3:]
+    if speaker == '-!-':
+        if ' '.join(words[1:4]) == 'changed nick to':
+            old_nick = words[0]
+            new_nick = words[4]
+            member_changed_nick(old_nick, new_nick)
+        elif ' '.join(words[1:3]) == 'has quit':
+            nick = words[0].split('(')[0]
+            member_remove(nick)
+        else:
+            log_warn('Ignorning unknown server ctrl message: {}'.format(
+                ' '.join(words)))
+    else:
+        log_warn('Ignoring server ctrl message with unknown speaker: {}'.format(
+            speaker))
+
+def channel_out_read_event(fd, mask):
+    line = fd.readline().decode('utf8')
+    tokens = line.split()
+    speaker = tokens[2]
+    words = tokens[3:]
+    if speaker == '-!-':
+        if ' '.join(words[1:3]) == 'has left':
+            nick = words[0].split('(')[0]
+            member_remove(nick)
+            log_debug('{} left ({})'.format(nick,len(members)))
+        elif ' '.join(words[1:3]) == 'has joined':
+            nick = words[0].split('(')[0]
+            member_add(nick)
+            log_debug('{} joined ({})'.format(nick,len(members)))
+        else:
+            log_warn('Ignoring unknown channel ctrl message: {}'.format(
+                ' '.join(words)))
+    elif speaker[0] != '<' or speaker[-1] != '>':
+        log_debug('Ignoring channel message with weird speaker: {}'.format(
+            speaker))
+    else:
+        speaker = speaker[1:-1].lower()
+        log_debug('{}: {}'.format(speaker, ' '.join(words)))
+        if is_highlight_spam(words):
+            akick(speaker, 'highlight spam')
 
 def main(s_dir, c_name):
     global members
     global server_dir
     global channel_name
+    global server_out
     global channel_out
     global notice_fd
     global debug_fd
     members = set()
     server_dir = s_dir
     channel_name = c_name
-    channel_out = subprocess.Popen(
-        ['tail','-F','-n','0',server_dir+'/'+channel_name+'/out'],
+    server_out = subprocess.Popen(
+        ['tail','-F','-n','0','{}/out'.format(server_dir)],
         stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    notice_fd = open(server_dir+'/'+channel_name+'/notice.log', 'a')
-    debug_fd = open(server_dir+'/'+channel_name+'/debug.log', 'a')
+    channel_out = subprocess.Popen(
+        ['tail','-F','-n','0','{}/{}/out'.format(server_dir,channel_name)],
+        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    notice_fd = open('{}/{}/notice.log'.format(server_dir,channel_name), 'a')
+    debug_fd = open('{}/{}/debug.log'.format(server_dir,channel_name), 'a')
+
     log_notice("Starting up bot")
     members = get_all_members()
+    log_debug(members)
     log_debug("Starting members: {}".format(len(members)))
+
+    selector.register(server_out.stdout, selectors.EVENT_READ,
+        server_out_read_event)
+    selector.register(channel_out.stdout, selectors.EVENT_READ,
+        channel_out_read_event)
+
     while True:
-        line = channel_out.stdout.readline().decode('utf8')
-        #print(line)
-        tokens = line.split()
-        speaker = tokens[2]
-        if speaker == '-!-':
-            if tokens[4] == 'has' and tokens[5] == 'left':
-                nick = tokens[3].split('(')[0]
-                log_debug('{} left'.format(nick))
-                members = set([ m for m in members if m != nick ])
-            elif tokens[4] == 'has' and tokens[5] == 'joined':
-                nick = tokens[3].split('(')[0]
-                log_debug('{} joined'.format(nick))
-                members.add(nick)
-            # nick changes come in on server_out, not channel_out
-            #elif tokens[4] == 'changed' and tokens[5] == 'nick' and tokens[6] == 'to':
-            #    old_nick, new_nick = tokens[3], tokens[7]
-            #    members = set([ m for m in members if m != old_nick ])
-            #    members.add(new_nick)
-            #    log_debug("{} changed to {}".format(old_nick, new_nick))
-            else:
-                log_debug("Ignoring unknown -!- message: {}".format(
-                    ' '.join(tokens[3:])))
-            continue
-        if speaker[0] != '<' or speaker[-1] != '>':
-            log_warn("Ignoring line with speaker {}".format(speaker))
-            continue
-        # remove < and >
-        speaker = speaker[1:]
-        speaker = speaker[:-1]
-        # remove @
-        if speaker[0] in nick_prefixes: speaker = speaker[1:]
-        words = tokens[3:]
-        if is_highlight_spam(words, members):
-            akick(speaker, "highlight spam")
-        if contains_banned_word(words):
-            akick(speaker)
-        #print(speaker+':',words)
-        log_debug(speaker+': '+' '.join(words))
-        #log_debug(str(members))
-        #print(members)
+        events = selector.select()
+        for key, mask in events:
+            callback = key.data
+            callback(key.fileobj, mask)
 
 if __name__=='__main__':
     signal.signal(signal.SIGINT, sigint)

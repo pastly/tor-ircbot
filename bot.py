3f58d3aab6af3b0e1f270ebdfbf1fb581a1fdb42
Matt Traudt <sirmatt@ksu.edu>
2017-03-13 20:04:18 -0400

    Add and use PastlyLogger

--- bot.py
+++ bot.py
@@ -5,9 +5,11 @@ import subprocess
 import sys
 from datetime import datetime
 from repeatedtimer import RepeatedTimer
+from pastlylogger import PastlyLogger
 #from threading import Lock
 
 selector = selectors.DefaultSelector()
+log = None
 
 # need to be all lowercase
 masters = ['pastly']
@@ -21,10 +23,6 @@ channel_name = None
 server_out = None
 channel_out = None
 privmsg_out = None
-debug_fd = None
-notice_fd = None
-warn_fd = None
-error_fd = None
 update_members_event = None
 
 banned_words = \
@@ -48,13 +46,13 @@ non_nick_punctuation.extend(nick_prefixes)
 def set_mention_limit(value):
     global mention_limit
     if len(value) != 1:
-        log_warn('Ignorning mention_limit: {}'.format(value))
+        log.warn('Ignorning mention_limit: {}'.format(value))
         return False
     value = value[0]
     try:
         value = int(value)
     except (TypeError, ValueError):
-        log_warn('Ignoring non-int mention_limit: {}'.format(value))
+        log.warn('Ignoring non-int mention_limit: {}'.format(value))
         return False
     mention_limit = value
     return True
@@ -65,13 +63,13 @@ def get_mention_limit():
 # True if successful
 def set_update_members_interval(value):
     if len(value) != 1:
-        log_warn('Ignoring update_members_interval: {}'.format(value))
+        log.warn('Ignoring update_members_interval: {}'.format(value))
         return False
     value = value[0]
     try:
         value = int(value)
     except (TypeError, ValueError):
-        log_warn('Ignoring non-int update_members_interval: {}'.format(value))
+        log.warn('Ignoring non-int update_members_interval: {}'.format(value))
     if value <= 0: value = 0
     update_members_event.interval = value
     update_members_event.stop()
@@ -90,46 +88,16 @@ options = {
 def usage(prog_name):
     print(prog_name,"<root-ii-server-dir> <channel>")
 
-def log(fd, s, level):
-    assert fd
-    ts = datetime.now()
-    fd.write('[{}] [{}] {}\n'.format(ts, level, s))
-
-def log_debug(s, level='debug'):
-    if debug_fd: return log(debug_fd, s, level)
-    return None
-
-def log_notice(s, level='notice'):
-    if notice_fd: return log(notice_fd, s, level)
-    else: return log_debug(s, level)
-
-def log_warn(s, level='warn'):
-    if warn_fd: return log(warn_fd, s, level)
-    else: return log_notice(s, level)
-
-def log_error(s, level='error'):
-    if error_fd: return log(error_fd, s, level)
-    else: return log_warn(s, level)
-
 def sigint(signum, stack_frame):
     global server_out
     global channel_out
     global privmsg_out
-    global debug_fd
-    global notice_fd
-    global warn_fd
-    global error_fd
     global update_members_event
-    log_notice("Shutting down bot due to signal")
+    log.notice("Shutting down bot due to signal")
     if server_out: server_out.terminate()
     if channel_out: channel_out.terminate()
     if privmsg_out: privmsg_out.terminate()
     server_out, channel_out, privmsg_out = None, None, None
-    if debug_fd: debug_fd.close()
-    if notice_fd: notice_fd.close()
-    if warn_fd: warn_fd.close()
-    if error_fd: error_fd.close()
-    debug_fd, notice_fd, warn_fd, error_fd = None, None, None, None
     if update_members_event: update_members_event.stop()
     update_members_event = None
     exit(0)
@@ -138,10 +106,7 @@ def sighup(signum, stack_frame):
     if server_out: server_out.stdout.flush()
     if channel_out: channel_out.stdout.flush()
     if privmsg_out: privmsg_out.stdout.flush()
-    if debug_fd: debug_fd.flush()
-    if notice_fd: notice_fd.flush()
-    if warn_fd: warn_fd.flush()
-    if error_fd: error_fd.flush()
+    log.flush()
 
 def privmsg(nick, message):
     with open(server_dir+'/in', 'w') as server_in:
@@ -152,11 +117,11 @@ def ping(nick):
 
 def akick(nick, reason=None):
     if not reason:
-        log_notice('akicking {}'.format(nick))
+        log.notice('akicking {}'.format(nick))
         privmsg('chanserv', 'akick {} add {}!*@*\n'.format(
             channel_name, nick))
     else:
-        log_notice('akicking {} for {}'.format(nick, reason))
+        log.notice('akicking {} for {}'.format(nick, reason))
         privmsg('chanserv', 'akick {} add {}!*@* {}\n'.format(
             channel_name, nick, reason))
 
@@ -177,7 +142,7 @@ def is_highlight_spam(words):
     for word in words:
         word = word.lstrip(punc).rstrip(punc)
         if word in members_: matches.add(word)
-    log_debug("{} nicks mentioned".format(len(matches)))
+    log.debug("{} nicks mentioned".format(len(matches)))
     if len(matches) > mention_limit: return True
     return False
 
@@ -193,7 +158,7 @@ def member_add(nick):
     old_len = len(members)
     members.add(nick.lower())
     if len(members) <= old_len:
-        log_warn('Adding {} to members didn\'t increase length'.format(nick))
+        log.warn('Adding {} to members didn\'t increase length'.format(nick))
     #members_lock.release()
 
 def member_remove(nick):
@@ -202,7 +167,7 @@ def member_remove(nick):
     old_len = len(members)
     members.discard(nick.lower())
     if len(members) >= old_len:
-        log_warn('Removing {} from members didn\'t decrease length'.format(
+        log.warn('Removing {} from members didn\'t decrease length'.format(
             nick))
     #members_lock.release()
 
@@ -216,18 +181,18 @@ def member_changed_nick(old_nick, new_nick):
     member_remove(old_nick)
     if len(members) < old_len:
         member_add(new_nick)
-        log_debug('{} --> {}'.format(old_nick, new_nick))
+        log.debug('{} --> {}'.format(old_nick, new_nick))
     #members_lock.release()
 
 def set_option(option, value):
     if option not in options:
-        log_warn('Ignoring unknown option: {}'.format(option))
+        log.warn('Ignoring unknown option: {}'.format(option))
         return False
     return options[option][0](value)
 
 def get_option(option):
     if option not in options:
-        log_warn('Ignoring unknown option: {}'.format(option))
+        log.warn('Ignoring unknown option: {}'.format(option))
         return None
     return options[option][1]()
 
@@ -245,10 +210,10 @@ def server_out_read_event(fd, mask):
             nick = words[0].split('(')[0]
             member_remove(nick)
             #members_lock.acquire()
-            log_debug('{} quit ({})'.format(nick,len(members)))
+            log.debug('{} quit ({})'.format(nick,len(members)))
             #members_lock.release()
         else:
-            log_warn('Ignorning unknown server ctrl message: {}'.format(
+            log.warn('Ignorning unknown server ctrl message: {}'.format(
                 ' '.join(words)))
     elif speaker in ['@','=']:
         if words[0] == channel_name:
@@ -256,7 +221,7 @@ def server_out_read_event(fd, mask):
                 if w[0] in nick_prefixes: member_add(w[1:])
                 else: member_add(w)
             #members_lock.acquire()
-            log_debug('Got {} more names. {} total'.format(len(words[1:]),
+            log.debug('Got {} more names. {} total'.format(len(words[1:]),
                 len(members)))
             #members_lock.release()
     elif speaker == channel_name:
@@ -267,10 +232,10 @@ def server_out_read_event(fd, mask):
         if ' '.join(words) == 'End of /NAMES list.':
             pass
         else:
-            log_debug('Ignoring unrecognized server ctrl message coming from '+
+            log.debug('Ignoring unrecognized server ctrl message coming from '+
                 '{}: {}'.format(channel_name, ' '.join(words)))
     else:
-        log_warn('Ignoring server ctrl message with unknown speaker: {}'.format(
+        log.warn('Ignoring server ctrl message with unknown speaker: {}'.format(
             speaker))
 
 def channel_out_read_event(fd, mask):
@@ -283,23 +248,23 @@ def channel_out_read_event(fd, mask):
             nick = words[0].split('(')[0]
             member_remove(nick)
             #members_lock.acquire()
-            log_debug('{} left ({})'.format(nick,len(members)))
+            log.debug('{} left ({})'.format(nick,len(members)))
             #members_lock.release()
         elif ' '.join(words[1:3]) == 'has joined':
             nick = words[0].split('(')[0]
             member_add(nick)
             #members_lock.acquire()
-            log_debug('{} joined ({})'.format(nick,len(members)))
+            log.debug('{} joined ({})'.format(nick,len(members)))
             #members_lock.release()
         else:
-            log_warn('Ignoring unknown channel ctrl message: {}'.format(
+            log.warn('Ignoring unknown channel ctrl message: {}'.format(
                 ' '.join(words)))
     elif speaker[0] != '<' or speaker[-1] != '>':
-        log_debug('Ignoring channel message with weird speaker: {}'.format(
+        log.debug('Ignoring channel message with weird speaker: {}'.format(
             speaker))
     else:
         speaker = speaker[1:-1].lower()
-        log_debug('<{}> {}'.format(speaker, ' '.join(words)))
+        log.debug('<{}> {}'.format(speaker, ' '.join(words)))
         if is_highlight_spam(words):
             akick(speaker, 'highlight spam')
             member_remove(speaker)
@@ -310,30 +275,30 @@ def privmsg_out_read_event(fd, mask):
     speaker = tokens[2]
     words = tokens[3:]
     if speaker[0] != '<' or speaker[-1] != '>':
-        log_warn('Ignoring privmsg with weird speaker: {}'.format(speaker))
+        log.warn('Ignoring privmsg with weird speaker: {}'.format(speaker))
         return
     speaker = speaker[1:-1].lower()
     if speaker not in masters:
-        log_warn('Ignoring privmsg from non-master: {}'.format(speaker))
+        log.warn('Ignoring privmsg from non-master: {}'.format(speaker))
         return
     if ' '.join(words) == 'ping':
-        log_debug('master {} pinged us'.format(speaker))
+        log.debug('master {} pinged us'.format(speaker))
         ping(speaker)
     elif words[0] == 'set':
         if len(words) < 3:
-            log_warn('Ignoring bad set command from {}: {}'.format(
+            log.warn('Ignoring bad set command from {}: {}'.format(
                 speaker, ' '.join(words)))
             return
         if not set_option(words[1], words[2:]):
             privmsg(speaker, 'Failed to set {} to {}'.format(
                 words[1], ' '.join(words[2:])))
         else:
-            log_notice('{} set {} to {}'.format(
+            log.notice('{} set {} to {}'.format(
                 speaker, words[1], ' '.join(words[2:])))
             privmsg(speaker, '{} is {}'.format(words[1], get_option(words[1])))
     elif words[0] == 'get':
         if len(words) != 2:
-            log_warn('Ignoring bad get command from {}: {}'.format(
+            log.warn('Ignoring bad get command from {}: {}'.format(
                 speaker, ' '.join(words)))
             return
         value = get_option(words[1])
@@ -341,12 +306,12 @@ def privmsg_out_read_event(fd, mask):
     elif words[0] == 'options':
         privmsg(speaker, ' '.join(options))
     else:
-        log_debug('master {} said "{}" but we don\'t have a response'.format(
+        log.debug('master {} said "{}" but we don\'t have a response'.format(
             speaker, ' '.join(words)))
 
 def ask_for_new_members():
     global members
-    log_debug('Clearing members set. Asking for members again')
+    log.debug('Clearing members set. Asking for members again')
     #members_lock.acquire()
     members = set()
     #members_lock.release()
@@ -362,11 +327,8 @@ def main(s_dir, c_name):
     global server_out
     global channel_out
     global privmsg_out
-    global debug_fd
-    global notice_fd
-    global warn_fd
-    global error_fd
     global update_members_event
+    global log
     server_dir = s_dir
     channel_name = c_name
     # bufsize=1 means line-based buffering. Perfect for IRC :)
@@ -383,12 +345,12 @@ def main(s_dir, c_name):
         server_dir,channel_name)],
         stdout=subprocess.PIPE,stderr=subprocess.PIPE,
         bufsize=1)
-    debug_fd = open('{}/{}/debug.log'.format(server_dir,channel_name), 'a')
-    #notice_fd = open('{}/{}/notice.log'.format(server_dir,channel_name), 'a')
-    #warn_fd = open('{}/{}/warn.log'.format(server_dir,channel_name), 'a')
-    #error_fd = open('{}/{}/error.log'.format(server_dir,channel_name), 'a')
 
-    log_notice("Starting up bot")
+    log = PastlyLogger(
+        debug='{}/{}/debug.log'.format(server_dir, channel_name),
+    )
+    log.notice("Starting up bot")
+
     update_members_event_callback()
 
     selector.register(server_out.stdout, selectors.EVENT_READ,
---
+++ pastlylogger.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+class PastlyLogger:
+    def __init__(self, error=None, warn=None, notice=None,
+        info=None, debug=None, overwrite=[]):
+
+        if error:
+            self.error_fd = open(error, 'w' if 'error' in overwrite else 'a')
+        else: self.error_fd = None
+        if warn:
+            self.warn_fd = open(warn, 'w' if 'warn' in overwrite else 'a')
+        else: self.warn_fd = None
+        if notice:
+            self.notice_fd = open(notice, 'w' if 'notice' in overwrite else 'a')
+        else: self.notice_fd = None
+        if info:
+            self.info_fd = open(info, 'w' if 'info' in overwrite else 'a')
+        else: self.info_fd = None
+        if debug:
+            self.debug_fd = open(debug, 'w' if 'debug' in overwrite else 'a')
+        else: self.debug_fd = None
+
+        self.debug('Creating PastlyLogger instance')
+
+    def __del__(self):
+        self.debug('Deleting PastlyLogger instance')
+        self.flush()
+        if self.error_fd: self.error_fd.close()
+        if self.warn_fd: self.warn_fd.close()
+        if self.notice_fd: self.notice_fd.close()
+        if self.info_fd: self.info_fd.close()
+        if self.debug_fd: self.debug_fd.close()
+        self.error_fd, self.warn_fd = None, None
+        self.notice_fd, self.info_fd, self.debug_fd = None, None, None
+
+    def _log_file(fd, s, level):
+        assert fd
+        ts = datetime.now()
+        fd.write('[{}] [{}] {}\n'.format(ts, level, s))
+
+    def flush(self):
+        if self.error_fd: self.error_fd.flush()
+        if self.warn_fd: self.warn_fd.flush()
+        if self.notice_fd: self.notice_fd.flush()
+        if self.info_fd: self.info_fd.flush()
+        if self.debug_fd: self.debug_fd.flush()
+
+    def debug(self, s, level='debug'):
+        if self.debug_fd: return PastlyLogger._log_file(self.debug_fd, s, level)
+        return None
+
+    def info(self, s, level='info'):
+        if self.info_fd: return PastlyLogger._log_file(self.info_fd, s, level)
+        else: return self.debug(s, level)
+
+    def notice(self, s, level='notice'):
+        if self.notice_fd: return PastlyLogger._log_file(self.notice_fd, s, level)
+        else: return self.info(s, level)
+
+    def warn(self, s, level='warn'):
+        if self.warn_fd: return PastlyLogger._log_file(self.warn_fd, s, level)
+        else: return self.notice(s, level)
+
+    def error(self, s, level='error'):
+        if self.error_fd: return PastlyLogger._log_file(self.error_fd, s, level)
+        else: return self.warn(s, level)

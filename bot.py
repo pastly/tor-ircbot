a9206e13e6e744c9864897c230b4527207a16191
Matt Traudt <sirmatt@ksu.edu>
2017-04-25 21:28:29 -0400

    Fix highlight spam detection not akicking correctly

--- bot.py
+++ bot.py
@@ -372,7 +372,14 @@ def channel_out_process_line(line):
         speaker = speaker[1:-1].lower()
         log.debug('<{}> {}'.format(speaker, ' '.join(words)))
         if get_enforce_highlight_spam() and is_highlight_spam(words):
-            akick(speaker, 'highlight spam')
+            if members.contains(nick=speaker):
+                m = members[speaker]
+                outbound_message_queue.add(akick, [
+                    '{}!*@*'.format(m.nick),
+                    'highlight spam ({}) (automatic)'.format(speaker)])
+                outbound_message_queue.add(akick, [
+                    '*!*@{}'.format(m.host),
+                    'highlight spam ({}) (automatic)'.format(speaker)])
 
 # must be called from the main process
 def privmsg_out_process_line(line):

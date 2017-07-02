1170cdf0824df7da4d8e916a0af01938dbfa07cb
Matt Traudt <sirmatt@ksu.edu>
2017-07-02 16:46:12 -0400

    Move signal stuff to a new file.

--- main.py
+++ main.py
@@ -4,32 +4,9 @@ import signal
 import time
 from multiprocessing import Event
 # my stuff
+from signalstuff import *
 from logprocess import LogProcess
 from watchfileprocess import WatchFileProcess
-
-def add_current_signals_to_stack(stack):
-    stack.append(signal.getsignal(signal.SIGINT))
-    stack.append(signal.getsignal(signal.SIGTERM))
-    stack.append(signal.getsignal(signal.SIGHUP))
-    return stack
-
-def pop_signals_from_stack(stack):
-    stack.pop()
-    stack.pop()
-    stack.pop()
-    signal.signal(signal.SIGINT, stack[-3])
-    signal.signal(signal.SIGTERM, stack[-2])
-    signal.signal(signal.SIGHUP, stack[-1])
-    return stack
-
-#def get_current_signals(stack):
-#    return stack[-3:]
-
-def set_signals(stack, INT, TERM, HUP):
-    signal.signal(signal.SIGINT, INT)
-    signal.signal(signal.SIGTERM, TERM)
-    signal.signal(signal.SIGHUP, HUP)
-    return add_current_signals_to_stack(stack)
 
 def main():
     global_state = {
---
+++ signalstuff.py
@@ -0,0 +1,25 @@
+import signal
+def add_current_signals_to_stack(stack):
+    stack.append(signal.getsignal(signal.SIGINT))
+    stack.append(signal.getsignal(signal.SIGTERM))
+    stack.append(signal.getsignal(signal.SIGHUP))
+    return stack
+
+def pop_signals_from_stack(stack):
+    stack.pop()
+    stack.pop()
+    stack.pop()
+    signal.signal(signal.SIGINT, stack[-3])
+    signal.signal(signal.SIGTERM, stack[-2])
+    signal.signal(signal.SIGHUP, stack[-1])
+    return stack
+
+#def get_current_signals(stack):
+#    return stack[-3:]
+
+def set_signals(stack, INT, TERM, HUP):
+    signal.signal(signal.SIGINT, INT)
+    signal.signal(signal.SIGTERM, TERM)
+    signal.signal(signal.SIGHUP, HUP)
+    return add_current_signals_to_stack(stack)
+

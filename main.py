dabd89867143d682b44632c0d762fcc6008576b5
Matt Traudt <sirmatt@ksu.edu>
2017-07-02 16:47:19 -0400

    Only formatting changes

--- main.py
+++ main.py
@@ -18,25 +18,24 @@ def main():
         },
         'events': {
             'is_shutting_down': Event(),
-        }
+        },
     }
+
     def sigint(signum, stack_frame):
         global_state['events']['is_shutting_down'].set()
         exit(0)
-
     def sigterm(signum, stack_frame):
         return sigint(signum, stack_frame)
-
     def sighup(signum, stack_frame):
         pass
 
     signal_stack = add_current_signals_to_stack([])
-    signal_stack = set_signals(signal_stack,
-        sigint, sigterm,  sighup)
+    signal_stack = set_signals(signal_stack, sigint, sigterm,  sighup)
     signal_stack = set_signals(signal_stack,
         signal.SIG_IGN,
         signal.SIG_IGN,
         signal.SIG_IGN)
+
     global_state['procs']['log'] = LogProcess(global_state,
         debug='/dev/stdout', overwrite=['debug']).start()
     global_state['procs']['watch_chan'] = WatchFileProcess('chan.txt',
@@ -48,7 +47,9 @@ def main():
     global_state['procs']['watch_chan'].start()
     #global_state['procs']['watch_serv'].start()
     #global_state['procs']['watch_priv'].start()
+
     signal_stack = pop_signals_from_stack(signal_stack)
+
     while True:
         time.sleep(10.0)
 

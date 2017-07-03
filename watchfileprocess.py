065980dbdd814ed617e3c38c6bc376efdab5853d
Matt Traudt <sirmatt@ksu.edu>
2017-07-03 16:59:47 -0400

    Add type to WatchFileProcess

--- watchfileprocess.py
+++ watchfileprocess.py
@@ -5,14 +5,16 @@ import time
 from random import random
 
 class WatchFileProcess(PBProcess):
-    def __init__(self, fname, global_state):
+    def __init__(self, fname, type, global_state):
         PBProcess.__init__(self, self._enter)
         self._fname = fname
+        self._type = type
         self.update_global_state(global_state)
 
     def _enter(self):
         log = self._log_proc
-        log.notice('Started WatchFileProcess {} instance'.format(self._fname))
+        log.notice('Started WatchFileProcess {} {} instance'.format(
+            self._type, self._fname))
         set_signals(self._ss, *get_default_signals(self._ss))
         sub = subprocess.Popen(['tail','-F','-n','0',self._fname],
             stdout=subprocess.PIPE,
@@ -32,7 +34,8 @@ class WatchFileProcess(PBProcess):
 
     def _shutdown(self):
         log = self._log_proc
-        if log: log.notice('WatchFileProcess {} going away'.format(self._fname))
+        if log: log.notice('WatchFileProcess {} {} going away'.format(
+            self._type, self._fname))
         return
 
     def update_global_state(self, gs):
@@ -41,4 +44,5 @@ class WatchFileProcess(PBProcess):
         self._ss = gs['signal_stack']
         if self._log_proc:
             self._log_proc.info(
-                'WatchFileProcess {} updated state'.format(self._fname))
+                'WatchFileProcess {} {} updated state'.format(
+                    self._type, self._fname))

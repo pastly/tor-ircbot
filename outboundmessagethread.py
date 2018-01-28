1cae1d960acd0e6c7ab7287090c13d0a18a16f1d
Matt Traudt <sirmatt@ksu.edu>
2018-01-28 17:02:01 -0500

    Note how OutboundMessageThread should be used
    Only via add(). After this commit, it'll be time to go fix that in the
    code.

--- outboundmessagethread.py
+++ outboundmessagethread.py
@@ -4,6 +4,19 @@ from pbthread import PBThread
 from actionqueue import ActionQueue
 
 class OutboundMessageThread(PBThread):
+    ''' If the bot is going to send a message/command to the IRC server, it
+    must do so through this class.
+
+    Use the add() function to add an outbound message/command. For example, to
+    pong the user 'foobar':
+
+    >>> omt = outbound_message_thread
+    >>> omt.add(omt.pong, ['foobar'])
+
+    More generally, specify the function you want to call as the first argument
+    to add(), any arguments you want to pass to it in as a list, and finally
+    any keyword arguments you want to pass to it as a dictionary.
+    '''
     pong_msgs = [ 'pong', 'PONG', 'POOOONG!!!!', 'JFC pong', 'No. Just stop.',
         'WTF do you want from me?', 'I\'m busy', 'moo', 'ACK', 'RST',
     ]
@@ -33,15 +46,31 @@ class OutboundMessageThread(PBThread):
         log.notice('OutboundMessageThread going away')
 
     def add(self, *args, **kwargs):
+        ''' Use this function to add outbound messages/commands '''
         self._action_queue.add(*args, **kwargs)
 
     def servmsg(self, message):
+        ''' Do not call this function directly. Pass it as an argument to add().
+
+        >>> omt = outbound_message_thread
+        >>> omt.add(omt.servmsg, ['/mode #foo +i'])
+        '''
         fname = os.path.join(self._server_dir, 'in')
         with open(fname, 'w') as server_in:
             server_in.write('{}\n'.format(message))
 
     def privmsg(self, nick, message):
+        ''' Do not call this function directly. Pass it as an argument to add().
+
+        >>> omt = outbound_message_thread
+        >>> omt.add(omt.privmsg, ['pastly', 'You left the stove on'])
+        '''
         self.servmsg('/privmsg {} {}'.format(nick, message))
 
     def pong(self, nick):
+        ''' Do not call this function directly. Pass it as an argument to add().
+
+        >>> omt = outbound_message_thread
+        >>> omt.add(omt.pong, ['pastly'])
+        '''
         self.privmsg(nick, random.choice(OutboundMessageThread.pong_msgs))

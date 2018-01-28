831578fa4ceb04621bfca7d5e2385dbefe717d70
Matt Traudt <sirmatt@ksu.edu>
2018-01-28 16:50:48 -0500

    More/better comments in ActionQueue

--- actionqueue.py
+++ actionqueue.py
@@ -2,8 +2,25 @@ import signal
 from time import time
 from queue import Empty, PriorityQueue, Queue
 class ActionQueue:
+    ''' A priority queue of functions to call in a controlling thread's main
+    loop.
+
+    Add functions to call with add(), optionally with arguments and/or keyword
+    arguments. A priority may also be specified. Lower priority items will be
+    handled first.
+
+    The controlling thread should repeatedly call loop_once(), most likely in a
+    (near) infinite main loop. It is up to the controlling thread to realize
+    if/when it is time to shutdown and quit. '''
 
     def __init__(self, long_timeout=10, time_between_actions_func=None):
+        ''' long_timeout is how long loop_once() will block when there is
+        nothing to do.
+
+        time_between_actions_func is a function called after every action to
+        determine how long we must wait before executing another action. The
+        function must take one argument: a variable holding its state. It
+        must return a tuple: (time_to_wait, new_state). '''
         self._incoming_queue = Queue()
         self._action_queue = PriorityQueue()
         # A function pointer that is called after every action to caclulate
@@ -28,9 +45,9 @@ class ActionQueue:
         # shut down very quickly
         self._long_timeout = long_timeout
 
-    # This function should be called by the main process to add an action to
-    # this process's priority queue through this inter-process FIFO.
-    # - func is the function to call in this process
+    # This function should be called by the main thread to add an action to
+    # this thread's priority queue through this inter-thread FIFO.
+    # - func is the function to call in this thread
     # - args is an optional list of arguments to pass to the function
     # - kwargs is an optional dictionary of arguments to pass to the function
     # - priority is ... the priority. Obviously. This is a min-priority queue.
@@ -39,10 +56,10 @@ class ActionQueue:
     #   seconds, and probably with fractional seconds). To put something in the
     #   queue with a little extra importance (like 5 seconds worth), set
     #   priority to time()-5. To put something in with upmost priority, add it
-    #   with priority == 0. If you never specify a priority, then this structure
-    #   is essentially a FIFO queue in its own process. NOTE: while priority
-    #   defaults to time, that does NOT mean setting priority == time()+5 means
-    #   the item will be processed after 5 seconds.
+    #   with priority == 0. If you never specify a priority, then this
+    #   ActionQueue is essentially a FIFO queue in its own thread. NOTE: while
+    #   priority defaults to time, that does NOT mean setting
+    #   priority == time()+5 means the item will be processed after 5 seconds.
     def add(self, func, args=None, kwargs=None, priority=None):
         if priority == None: priority = time()
         self._incoming_queue.put( (priority, (func, args, kwargs)) )
